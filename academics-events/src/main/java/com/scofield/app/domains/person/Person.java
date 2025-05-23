fc21ef811a4dee3f14302a1f22b1d4d04d753215
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2025-05-23 21:54:56 +0000

    Add class Person in Domain

---
+++ academics-events/src/main/java/com/scofield/app/domains/person/Person.java
@@ -0,0 +1,51 @@
+package com.scofield.app.domains.person;
+
+public class Person{
+    private static long peopleAmount;
+    protected long id;
+    protected String name;
+    protected String cpf;
+
+    public Person(String name, String cpf){
+        validate(name, cpf);
+
+        this.id = ++peopleAmount;
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    private void validate(String name, String cpf){
+        if(name.isEmpty() ||  name == null){
+            throw new IllegalArgumentException("Name can't be null or empty");
+        }
+        if(!validateCpf(cpf)){
+            throw new IllegalArgumentException("Invalid CPF");
+        }
+    }
+
+    public static boolean validateCpf(String cpf) {
+        if (cpf == null || !cpf.count() == 11) {
+            return false;
+        }
+
+        if (cpf.chars().distinct().count() == 1) {
+            return false;
+        }
+
+        int soma1 = 0;
+        for (int i = 0; i < 9; i++) {
+            soma1 += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int digito1 = 11 - (soma1 % 11);
+        if (digito1 >= 10) digito1 = 0;
+
+        int soma2 = 0;
+        for (int i = 0; i < 10; i++) {
+            soma2 += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        int digito2 = 11 - (soma2 % 11);
+        if (digito2 >= 10) digito2 = 0;
+            
+        return cpf.charAt(9) - '0' == digito1 && cpf.charAt(10) - '0' == digito2;
+    }
+}
