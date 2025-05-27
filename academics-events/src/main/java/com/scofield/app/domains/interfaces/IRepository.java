4279e334370efdd36bcdfb0fc29956053c2b270e
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2025-05-27 16:53:54 +0000

    Fix IRepository to accept any type on returns

--- academics-events/src/main/java/com/scofield/app/domains/interfaces/IRepository.java
+++ academics-events/src/main/java/com/scofield/app/domains/interfaces/IRepository.java
@@ -2,8 +2,8 @@ package com.scofield.app.domains.interfaces;
 
 import java.util.HashSet;
 
-public interface IRepository {
-    public HashSet<T> GetAll();
-    public T GetById(long id);
-    public boolean Register(Object o);
+public interface IRepository<T> {
+    public HashSet<T> getAll();
+    public T getById(long id);
+    public boolean register(Object o);
 }
