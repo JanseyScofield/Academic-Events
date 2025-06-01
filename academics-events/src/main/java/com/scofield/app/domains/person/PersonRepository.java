9c3cd181b3c104a95c89988f39b1bf321cf62448
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2025-06-01 20:35:11 +0000

    Add PersonRepository

---
+++ academics-events/src/main/java/com/scofield/app/domains/person/PersonRepository.java
@@ -0,0 +1,25 @@
+package com.scofield.app.domains.person;
+
+import java.util.HashSet;
+import com.scofield.app.domains.interfaces.IRepository;
+
+public class PersonRepository implements IRepository<Person>{
+    private HashSet<Person> personHasSet = new HashSet<Person>();
+
+    public HashSet<Person> getAll(){
+        return this.personHasSet;
+    }
+
+    public Person getById(long id){
+        return personHasSet.stream()
+                            .filter(p -> p.getId() == id)
+                            .findFirst()
+                            .orElse(null);                   
+    }
+
+    public boolean register(Object o){
+        if(!(o instanceof Person) || o == null) return false;
+        personHasSet.add((Person) o);
+        return true;
+    }
+}
