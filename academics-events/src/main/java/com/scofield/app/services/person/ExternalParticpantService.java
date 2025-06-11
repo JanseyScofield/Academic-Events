319af0e8729278b2ed57c5eaebbb8d5f56d18498
JanseyScofield <janscofield10@gmail.com>
2025-06-11 11:50:57 -0300

    Add ExternalParticipantService

---
+++ academics-events/src/main/java/com/scofield/app/services/person/ExternalParticpantService.java
@@ -0,0 +1,28 @@
+package com.scofield.app.services.person;
+
+
+import com.scofield.app.domains.person.PersonRepository;
+import com.scofield.app.domains.person.Student;
+import com.scofield.app.domains.person.ExternalParticipant;
+import com.scofield.app.services.abstratcs.Service;
+
+public class ExternalParticpantService extends Service<ExternalParticipant>{
+    public ExternalParticpantService(PersonRepository repository ){
+        super(repository);
+    }
+
+    @Override
+    public ExternalParticipant register(String name, String cpf, Object authorizantionToken){
+        try{
+            if(!(authorizantionToken instanceof String)){
+                throw new IllegalArgumentException("To register an external participant, authorizantion token must be an string");
+            }
+            ExternalParticipant newExternalParticipant = new ExternalParticipant(name,  cpf, (String)authorizantionToken);
+            repository.register(newExternalParticipant);
+            return newExternalParticipant;
+        }catch (IllegalArgumentException ex){
+            throw new IllegalArgumentException("Error to register an student: " + ex.getMessage());
+        }
+        
+    }
+}
